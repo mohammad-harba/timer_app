9dd974d5a3aa916a0a178bd8258a549cedcba856
Mohammad Harba <mharba90@gmail.com>
2022-06-06 15:57:51 +0200

    timer working

---
+++ lib/home_screen.dart
@@ -0,0 +1,59 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Duration duration = Duration();
+  var counter = 86500;
+  String twoDigits(int n) => n.toString().padLeft(2, '0');
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() {
+    Timer.periodic(const Duration(seconds: 1), ((timer) {
+      counter += 1;
+      duration = Duration(seconds: counter);
+      setState(() {});
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final days = twoDigits(duration.inDays);
+    final hours = twoDigits(duration.inHours.remainder(24));
+    final minutes = twoDigits(duration.inMinutes.remainder(60));
+    final seconds = twoDigits(duration.inSeconds.remainder(60));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'days : hours : mins : secs',
+              style: Theme.of(context).textTheme.headline5,
+            ),
+            const Divider(
+              height: 50,
+            ),
+            Text(
+              "$days : $hours : $minutes : $seconds",
+              //'$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
